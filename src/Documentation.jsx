2d23a8896e04de68810ebc71d9312127d790946c
Blindtroop <101224575+Blindtroop@users.noreply.github.com>
2023-02-08 23:06:03 +0300

    Add files via upload

---
+++ src/Documentation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Documentation() {
+    return (
+      <div>
+        <h1>Documentation</h1>
+        <button>This is a button</button>
+      </div>
+    );
+  }
+  export default Documentation;
